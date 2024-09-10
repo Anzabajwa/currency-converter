ab83cd51a41ed46a3d0d81d946021c47aad48bcf
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-09-10 07:25:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+import inquirer from 'inquirer';
+let Conversion = {
+    "PKR": {
+        "USD": 0.0036,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    //gbp
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 350,
+        "GBP": 1
+    },
+    //usd
+    "USD": {
+        "PKR": 277.58,
+        "GBP": 0.83,
+        "USD": 1,
+    },
+};
+//  step 2 
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your  currency?",
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your conversion currency?",
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your conversion amount"
+    }
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    let result = Conversion[from][to] * amount;
+    console.log(`your conversion from ${from} to ${to} is ${result}`);
+}
+else {
+    console.log("invalid input");
+}
